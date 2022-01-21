c17221200b3a38cd66d59be953c39b10009428a5
Joonas Koivunen <joonas@equilibrium.co>
2022-01-21 11:44:15 +0200

    chore(bench): reproducable report

--- bench_report.sh
+++ bench_report.sh
@@ -18,40 +18,46 @@ cargo build --release -p pathfinder --example merkle_global_tree
 
 echo "contract commitment root"
 head -2 output.bench.storage
-a=$(/usr/bin/time -f '%E wall, %S kernel, %U user' -o time.pathfinder.storage target/release/examples/merkle_storage_tree.old < output.bench.storage 2>/dev/null)
+a=$(/usr/bin/time -f '%E wall, %S kernel, %U user' -o time.pathfinder.storage.old target/release/examples/merkle_storage_tree.old < output.bench.storage 2>/dev/null)
 b=$(/usr/bin/time -f '%E wall, %S kernel, %U user' -o time.pathfinder.storage target/release/examples/merkle_storage_tree < output.bench.storage 2>/dev/null)
 c=$(/usr/bin/time -f '%E wall, %S kernel, %U user' -o time.cairolang.storage py/.venv/bin/python py/src/generate_test_storage_tree.py < output.bench.storage 2>/dev/null)
 
-if [[ "$a" == "$b" -a "$b" == "$c" ]]; then
+if [[ "$a" == "$b" ]] && [[ "$b" == "$c" ]]; then
 	echo "roots match: $a"
 else
 	echo "mismatch storage $a vs. $b vs. $c"
 	exit 1
 fi
 
-echo -n "pathfinder: "
+echo -n "pathfinder.old: "
+cat time.pathfinder.storage.old
+
+echo -n "pathfinder:     "
 cat time.pathfinder.storage
 
-echo -n "cairolang:  "
+echo -n "cairolang:      "
 cat time.cairolang.storage
 
 echo
 echo "global commitment root"
 
 head -2 output.bench.global
-a=$(/usr/bin/time -f '%E wall, %S kernel, %U user' -o time.pathfinder.global target/release/examples/merkle_global_tree.old < output.bench.global 2>/dev/null)
+a=$(/usr/bin/time -f '%E wall, %S kernel, %U user' -o time.pathfinder.global.old target/release/examples/merkle_global_tree.old < output.bench.global 2>/dev/null)
 b=$(/usr/bin/time -f '%E wall, %S kernel, %U user' -o time.pathfinder.global target/release/examples/merkle_global_tree < output.bench.global 2>/dev/null)
 c=$(/usr/bin/time -f '%E wall, %S kernel, %U user' -o time.cairolang.global py/.venv/bin/python py/src/generate_test_global_tree.py < output.bench.global 2>/dev/null)
 
-if [[ "$a" == "$b" -a "$b" == "$c" ]]; then
+if [[ "$a" == "$b" ]] && [[ "$b" == "$c" ]]; then
 	echo "roots match: $a"
 else
 	echo "mismatch $a vs. $b vs. $c"
 	exit 1
 fi
 
-echo -n "pathfinder: "
+echo -n "pathfinder.old: "
+cat time.pathfinder.global.old
+
+echo -n "pathfinder:     "
 cat time.pathfinder.global
 
-echo -n "cairolang:  "
+echo -n "cairolang:      "
 cat time.cairolang.global
