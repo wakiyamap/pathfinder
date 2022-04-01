f790c13ab7101ca5c04771db20688bece39f949a
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2022-04-01 20:23:15 +0200

    feat: test 2

--- crates/pathfinder/src/bin/goose.rs
+++ crates/pathfinder/src/bin/goose.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use goose::prelude::*;
 
 async fn syncing(user: &mut GooseUser) -> GooseTaskResult {
@@ -12,25 +13,25 @@ async fn syncing(user: &mut GooseUser) -> GooseTaskResult {
     Ok(())
 }
 
-// async fn transaction_hash(user: &mut GooseUser) -> GooseTaskResult {
-//     let json = &serde_json::json!({
-//         "jsonrpc": "2.0",
-//         "id": "0",
-//         "method": "starknet_getTransactionByHash",
-//         "params": {
-//             "transaction_hash": "0x6bc8a636965aabff8637eba5df9775bfe79858a51458dbcf8c6d55d584e90f1",
-//         }
-//     });
+async fn transaction_hash(user: &mut GooseUser) -> GooseTaskResult {
+    let json = &serde_json::json!({
+        "jsonrpc": "2.0",
+        "id": "0",
+        "method": "starknet_getTransactionByHash",
+        "params": {
+            "transaction_hash": "0x6bc8a636965aabff8637eba5df9775bfe79858a51458dbcf8c6d55d584e90f1",
+        }
+    });
 
-//     user.post_json("", json).await?;
+    user.post_json("", json).await?;
 
-//     Ok(())
-// }
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), GooseError> {
     GooseAttack::initialize()?
-        .register_taskset(taskset!("pathfinder").register_task(task!(syncing)))
+        .register_taskset(taskset!("pathfinder").register_task(task!(transaction_hash)))
         .execute()
         .await?
         .print();
